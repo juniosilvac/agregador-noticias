57c9d19eb2a571feedba41781273cba99f1e11d1
Junio Silva <junio.silvac@gmail.com>
2018-05-03 02:26:38 -0300

    Refatorado o acesso aos dados

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -3,8 +3,9 @@ module.exports = function(application) {
   application.get('/noticia', function(req, res){
 
     var connection = application.config.dbConnection();
-    var noticiasModel = application.app.models.noticiasModel;
-    noticiasModel.getNoticia(connection, function(error, result){
+    var noticiasModel = new application.app.models.NoticiasDAO(connection);
+
+    noticiasModel.getNoticia(function(error, result){
       res.render("noticias/noticia",{noticia: result});    
     });  
   });
